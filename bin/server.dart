99f9f93e6884c7836551eb615c1ecf4a032a8df9
Dilawar Sumra <admin@ecosperityindia.com>
2023-05-30 11:57:29 +0530

    first commit

---
+++ bin/server.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+
+void main(List<String> arguments) async {
+  final ip = InternetAddress.anyIPv4;
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+
+  Handler _wsHandler = webSocketHandler((webSocket) {
+    webSocket.stream.listen((message) {
+      webSocket.sink.add("echo $message");
+    });
+  });
+
+  final _router = Router()..get('/api/v1/users/ws', _wsHandler);
+  final wsHandler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  final wsServer = await serve(wsHandler, ip, port);
+  print('Server listening on port ${wsServer.port}');
+}
