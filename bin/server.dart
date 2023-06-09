2b8b02a7c654d8101fe2bee7c99659600270cea4
Dilawar Sumra <admin@ecosperityindia.com>
2023-06-09 18:13:10 +0530

    first commit

--- bin/server.dart
+++ bin/server.dart
@@ -4,9 +4,12 @@ import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf_web_socket/shelf_web_socket.dart';
 
+import 'websocket_chat/websocket_chat.dart';
+
 void main(List<String> arguments) async {
   final ip = InternetAddress.anyIPv4;
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final chatWebSocket = WebSocketChat.create();
 
   Handler _wsHandler = webSocketHandler((webSocket) {
     webSocket.stream.listen((message) {
@@ -16,7 +19,11 @@ void main(List<String> arguments) async {
   });
 
   final _router = Router()..get('/api/v1/users/ws', _wsHandler);
-  final wsHandler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
-  final wsServer = await serve(wsHandler, ip, port);
+  final wsHandler = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(chatWebSocket.middleware)
+      .addHandler(_router);
+  final wsServer = await serve(wsHandler, ip, port)
+      .whenComplete(() async => await chatWebSocket.dispose());
   print('Server listening on port ${wsServer.port}');
 }
